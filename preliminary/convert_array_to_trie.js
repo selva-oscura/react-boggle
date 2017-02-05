51b7d044a295084442fb104e8a00586fd41c8d3a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-04 22:30:25 -0800

    created isInDictionary function for testing if a word is in the trie (function will later be moved, but is being used here to test that trie is working correctly)

--- preliminary/convert_array_to_trie.js
+++ preliminary/convert_array_to_trie.js
@@ -15,6 +15,30 @@ const arrayToTrie = (array) => {
 	});
 }
 
+const isInDictionary = (dict, word) => {
+	let response = false;
+	let node = dict;
+	let wordLength = word.length;
+	for(let i = 0; i<wordLength; i++){
+		if(node[word[i]]){
+			node = node[word[i]];
+			if(i === wordLength-1){
+				if(node['complete']){
+					response = true;
+				}
+			}
+		}else{
+			console.log('exceeded')
+			return response;
+		}
+	}
+	return response;
+}
 let testArray = ['cat', 'car', 'cab', 'bat'];
 arrayToTrie(testArray);
 console.log('trie', trie);
+
+console.log('isInDictionary for cat', isInDictionary(trie, 'cat'));
+console.log('isInDictionary for ca', isInDictionary(trie, 'ca'));
+console.log('isInDictionary for cay', isInDictionary(trie, 'cay'));
+console.log('isInDictionary for catamount', isInDictionary(trie, 'catamount'));
