69ffa280144a8e05ab6a1f3e2e3dc1d54f82c775
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-04 23:27:50 -0800

    checked that trie created from TWL06 imports correctly and works properly with isInDictionary function

--- preliminary/convert_array_to_trie.js
+++ preliminary/convert_array_to_trie.js
@@ -38,31 +38,32 @@ const isInDictionary = (dict, word) => {
 	return response;
 }
 
-arrayToTrie(twl06);
+// arrayToTrie(twl06);
 
-console.log('trie', trie);
+// console.log('trie', trie);
 
-console.log('isInDictionary for cat', isInDictionary(trie, 'cat'));
-console.log('isInDictionary for cab', isInDictionary(trie, 'cab'));
-console.log('isInDictionary for cb', isInDictionary(trie, 'cb'));
-console.log('isInDictionary for ca', isInDictionary(trie, 'ca'));
-console.log('isInDictionary for cay', isInDictionary(trie, 'cay'));
-console.log('isInDictionary for catamount', isInDictionary(trie, 'catamount'));
+// console.log('isInDictionary for cat', isInDictionary(trie, 'cat'));
+// console.log('isInDictionary for cab', isInDictionary(trie, 'cab'));
+// console.log('isInDictionary for cb', isInDictionary(trie, 'cb'));
+// console.log('isInDictionary for ca', isInDictionary(trie, 'ca'));
+// console.log('isInDictionary for cay', isInDictionary(trie, 'cay'));
+// console.log('isInDictionary for catamount', isInDictionary(trie, 'catamount'));
 
-fs.writeFile("./twl06.json", JSON.stringify(trie), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-}); 
+// fs.writeFile("./twl06.json", JSON.stringify(trie), function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+//     console.log("The file was saved!");
+// }); 
 
 
-// const testDict = require('./twl06.json');
+const testDict = require('./twl06.json');
 // console.log(testDict);
 
-// console.log('isInDictionary for cat', isInDictionary(testDict, 'cat'));
-// console.log('isInDictionary for cab', isInDictionary(testDict, 'cab'));
-// console.log('isInDictionary for cb', isInDictionary(testDict, 'cb'));
-// console.log('isInDictionary for ca', isInDictionary(testDict, 'ca'));
-// console.log('isInDictionary for cay', isInDictionary(testDict, 'cay'));
-// console.log('isInDictionary for catamount', isInDictionary(testDict, 'catamount'));
+console.log('isInDictionary for aa (should be true)', isInDictionary(testDict, 'aa'));
+console.log('isInDictionary for ca (should be false)', isInDictionary(testDict, 'ca'));
+console.log('isInDictionary for cab (should be true)', isInDictionary(testDict, 'cab'));
+console.log('isInDictionary for cat (should be true)', isInDictionary(testDict, 'cat'));
+console.log('isInDictionary for cb (should be false)', isInDictionary(testDict, 'cb'));
+console.log('isInDictionary for cay (should be true)', isInDictionary(testDict, 'cay'));
+console.log('isInDictionary for catamount (should be true)', isInDictionary(testDict, 'catamount'));
