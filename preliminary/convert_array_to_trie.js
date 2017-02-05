7c9b5130fa3c663fc5b85f515e0bbe157df64a01
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-04 23:36:01 -0800

    verified that pruned TWL06 dictionary trie excludes words < 3 letters long

--- preliminary/convert_array_to_trie.js
+++ preliminary/convert_array_to_trie.js
@@ -43,7 +43,7 @@ const isInDictionary = (dict, word) => {
 	return response;
 }
 
-arrayToTrie(twl06);
+// arrayToTrie(twl06);
 
 // console.log('trie', trie);
 
@@ -54,22 +54,22 @@ arrayToTrie(twl06);
 // console.log('isInDictionary for cay', isInDictionary(trie, 'cay'));
 // console.log('isInDictionary for catamount', isInDictionary(trie, 'catamount'));
 
-fs.writeFile("./twl06.json", JSON.stringify(trie), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-}); 
+// fs.writeFile("./twl06.json", JSON.stringify(trie), function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+//     console.log("The file was saved!");
+// }); 
 
 
-// const testDict = require('./twl06.json');
-// // console.log(testDict);
+const testDict = require('./twl06.json');
+// console.log(testDict);
 
-// console.log('isInDictionary for aa (should be false)', isInDictionary(testDict, 'aa'));
-// console.log('isInDictionary for ca (should be false)', isInDictionary(testDict, 'ca'));
-// console.log('isInDictionary for cab (should be true)', isInDictionary(testDict, 'cab'));
-// console.log('isInDictionary for cat (should be true)', isInDictionary(testDict, 'cat'));
-// console.log('isInDictionary for cb (should be false)', isInDictionary(testDict, 'cb'));
-// console.log('isInDictionary for cay (should be true)', isInDictionary(testDict, 'cay'));
-// console.log('isInDictionary for catamount (should be true)', isInDictionary(testDict, 'catamount'));
-// console.log('isInDictionary for hi (should be false, due to pruning)', isInDictionary(testDict, 'hi'));
+console.log('isInDictionary for aa (should be false)', isInDictionary(testDict, 'aa'));
+console.log('isInDictionary for ca (should be false)', isInDictionary(testDict, 'ca'));
+console.log('isInDictionary for cab (should be true)', isInDictionary(testDict, 'cab'));
+console.log('isInDictionary for cat (should be true)', isInDictionary(testDict, 'cat'));
+console.log('isInDictionary for cb (should be false)', isInDictionary(testDict, 'cb'));
+console.log('isInDictionary for cay (should be true)', isInDictionary(testDict, 'cay'));
+console.log('isInDictionary for catamount (should be true)', isInDictionary(testDict, 'catamount'));
+console.log('isInDictionary for hi (should be false, due to pruning)', isInDictionary(testDict, 'hi'));
